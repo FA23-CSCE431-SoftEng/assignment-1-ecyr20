338e158b19302c276560e827428a67e001bfbd7e
ecyr20 <ecyr20@tamu.edu>
2023-09-05 14:31:00 -0500

    New Failing Tests

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -6,12 +6,29 @@ RSpec.describe Book, type: :model do
     described_class.new(title: 'harry potter')
   end
 
+#, :author 'J.K. Rowling', :price 10.99, :publishedDate '2004-08-05'
+
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without a title' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.date = nil
     expect(subject).not_to be_valid
   end
 end
